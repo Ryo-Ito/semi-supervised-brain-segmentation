eb2e00a0de1fc600be113bc31583899ac0a3dd01
Ryo-Ito <r0735nj5058@gmail.com>
2017-05-28 14:57:54 +0900

    wrote code updating displacement

--- em_displacement.py
+++ em_displacement.py
@@ -9,14 +9,14 @@ import pandas as pd
 from load import load_nifti
 
 
-def update_label(df, n_classes):
+def update_label(df, n_classes, suffix):
 
     for label_path, label_warped_path, subject in zip(df["label"], df["label_warped"], df["subject"]):
         if label_warped_path == label_warped_path:
             proba = load_nifti(
                 os.path.join(
                     os.path.dirname(label_path),
-                    subject + "_segTRI_proba.nii.gz"
+                    subject + suffix
                 )
             )
             noisy_label, affine = load_nifti(label_warped_path, with_affine=True)
@@ -36,27 +36,48 @@ def update_label(df, n_classes):
             nib.save(nib.Nifti1Image(posterior.astype(np.float32), affine), label_path)
 
 
+def split(inputfile, *outputfile):
+    img = nib.load(inputfile)
+    data = img.get_data()
+    data = data.transpose(3, 0, 1, 2)
+    for d, out in zip(data[1:], outputfile):
+        nib.save(nib.Nifti1Image(d, img.affine), out)
+
+
+def merge(outputfile, *inputfile):
+    data = []
+    for infile in inputfile:
+        img = nib.load(infile)
+        data.append(img.get_data())
+    backgroud = 1 - np.sum(data, axis=0)
+    assert (backgroud >= 0.).all() and (backgroud <= 1.).all()
+    data.insert(0, backgroud)
+    data = np.asarray(data)
+    data = data.transpose(1, 2, 3, 0)
+    nib.save(nib.Nifti1Image(data, img.affine), outputfile)
+
+
 def update_displacement(df):
 
-    for label_path, label_warped_path, subject in zip(df["label"], df["label_warped_path"], df["subject"]):
+    for label_path, label_warped_path in zip(df["label"], df["label_warped_path"]):
         if label_warped_path == label_warped_path:
+            fixed = ["f{}.nii.gz".format(i) for i in range(1, 4)]
+            moving = ["m{}.nii.gz".format(i) for i in range(1, 4)]
+            split(label_path, *fixed)
+            split(label_warped_path, *moving)
             cmd = (
-                "ANTS 3 -m MSQ[{}, {}, 1, 2] -m MSQ[{}, {}, 1, 2] -m MSQ[{}, {}, 1, 2] -i 50x20x10 -t SyN[0.3] -r Gauss[3,0.5] -o tmp"
-                .format(
-                    label_path,
-                    label_warped_path
-                )
+                "ANTS 3 -i 50x20x10 -t SyN[0.3] -r Gauss[3,0.5] -o tmp"
+                "-m MSQ[{0[0]}, {1[0]}, 1, 2] -m MSQ[{0[1]}, {1[1]}, 1, 2] -m MSQ[{0[2]}, {1[2]}, 1, 2]"
+                .format(fixed, moving)
             )
             os.system(cmd)
-            cmd = (
-                "antsApplyTransforms -d 3 -i {} -r {} -o {} -t tmpAffine.txt tmpWarp.nii.gz -n NearestNeighbor"
-                .format(
-                    label_warped_path,
-                    label_warped_path,
-                    label_warped_path
+            for m in moving:
+                cmd = (
+                    "antsApplyTransforms -d 3 -i {} -r {} -o {} -t tmpAffine.txt tmpWarp.nii.gz -n NearestNeighbor"
+                    .format(m, label_warped_path, m)
                 )
-            )
-            os.system(cmd)
+                os.system(cmd)
+            merge(label_warped_path, *moving)
 
 
 parser = argparse.ArgumentParser(description="train VoxResNet with EM alg.")
@@ -124,19 +145,25 @@ cmd_segment_proba = (
     .format(args.input_file, args.shape[0], args.shape[1], args.shape[2], args.gpu)
 )
 
-os.system(cmd_train_network + "-o weight_0.npz")
+output_directory = os.path.dirname(args.out)
+root, ext = os.path.dirname(args.out)
+
+output_file = root + "_0" + ext
+if not os.path.exists(output_file):
+    os.system(cmd_train_network + "-o {}".format(output_file))
 
 for i in range(1, args.em_step + 1):
     print("=" * 80)
     print("EM step {0:02d}".format(i))
 
     # E step
-    os.system(cmd_segment_proba + "-m weight_{}.npz".format(i - 1))
-    update_label(train_df, n_classes)
-    raise ValueError
+    suffix = "_segTRI_proba_{}.nii.gz".format(i)
+    os.system(
+        cmd_segment_proba + "-m {0} -o {1}".format(output_file, suffix)
+    )
+    update_label(train_df, n_classes, suffix)
 
     # M step
     update_displacement(train_df)
-    os.system(cmd_train_network + "-o weight_{}.npz".format(i))
-
-os.system("mv weight_{}.npz {}".format(args.em_step, args.out))
+    output_file = root + "_{}".format(i) + ext
+    os.system(cmd_train_network + "-o {}".format(output_file))
