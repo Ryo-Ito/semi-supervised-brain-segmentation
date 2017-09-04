fbe987e8420813b504b1b4b2bd645f974205cccc
Ryo-Ito <r0735nj5058@gmail.com>
2017-09-04 17:03:29 +0900

    replaced key to access the preprocessed images

--- utils.py
+++ utils.py
@@ -53,7 +53,7 @@ def load_sample(df, n, input_shape, output_shape):
         indices = np.random.choice(N, n, replace=True, p=weights)
     else:
         indices = np.random.choice(N, n, replace=True)
-    image_files = df["image"][indices]
+    image_files = df["preprocessed"][indices]
     label_files = df["label"][indices]
     images = []
     labels = []
@@ -76,64 +76,6 @@ def load_sample(df, n, input_shape, output_shape):
         image_patch = image[input_slices]
         label_patch = label[output_slices]
         image_patch = image_patch.transpose(3, 0, 1, 2)
-        images.append(image_patch)
-        labels.append(label_patch)
-    images = np.array(images)
-    labels = np.array(labels)
-    return images, labels
-
-
-def load_sample_onehot(df, n, input_shape, output_shape):
-    """
-    randomly sample patch images from DataFrame
-    Parameters
-    ----------
-    df : pd.DataFrame
-        DataFrame containing name of image files
-    n : int
-        number of patches to extract
-    input_shape : list
-        shape of input patches to extract
-    output_shape : list
-        shape of output patches to extract
-    Returns
-    -------
-    images : (n, n_channels, input_shape[0], input_shape[1], ...) ndarray
-        input patches
-    labels : (n, output_shape[0], output_shape[1], ...) ndarray
-        label patches
-    """
-    N = len(df)
-    if "weight" in list(df):
-        weights = np.asarray(df["weight"])
-        weights /= np.sum(weights)
-        indices = np.random.choice(N, n, replace=True, p=weights)
-    else:
-        indices = np.random.choice(N, n, replace=True)
-    image_files = df["image"][indices]
-    label_files = df["onehot"][indices]
-    images = []
-    labels = []
-    for image_file, label_file in zip(image_files, label_files):
-        image = load_nifti(image_file)
-        label = load_nifti(label_file)
-        mask = np.int32(label[..., 0] > 0)
-        slices = [slice(len_ // 2, -len_ // 2) for len_ in input_shape]
-        mask[slices] *= 2
-        indices = np.where(mask > 1.5)
-        i = np.random.choice(len(indices[0]))
-        input_slices = [
-            slice(index[i] - len_ // 2, index[i] + len_ // 2)
-            for index, len_ in zip(indices, input_shape)
-        ]
-        output_slices = [
-            slice(index[i] - len_ // 2, index[i] + len_ // 2)
-            for index, len_ in zip(indices, output_shape)
-        ]
-        image_patch = image[input_slices]
-        label_patch = label[output_slices]
-        image_patch = image_patch.transpose(3, 0, 1, 2)
-        label_patch = label_patch.transpose(3, 0, 1, 2)
         images.append(image_patch)
         labels.append(label_patch)
     images = np.array(images)
